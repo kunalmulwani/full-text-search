f2e7ced517f611421c1c16d2029f8a486c9e6013
kunalmulwani <kunalmulwani@gmail.com>
2020-08-15 15:58:36 -0700

    Deleted pre index test code and modified data file path

--- main.go
+++ main.go
@@ -5,24 +5,10 @@ import (
 )
 
 func main() {
-  filePath := "/home/kunal/projects/full-text-search/enwiki-latest-abstract1.xml"
+  filePath := "enwiki-latest-abstract1.xml"
   docs, _ := loadDocuments(filePath)
   fmt.Println("Documents loaded")
   fmt.Println("Corupus length", len(docs))
-  /*
-  * Testing part 
-  catDocs := search(docs, "Caterpillar")
-  fmt.Println("search completed. Results size", len(catDocs))
-  for _, doc := range catDocs {
-    fmt.Println(doc.Title)
-    if doc.Title == "Wikipedia: Caterpillar" {
-      fmt.Println("Found")
-      break
-    }
-  }
-  
-  fmt.Println(analyze("A donut on a glass plate. Only the donuts."))
-  */
   idx := make(index)
   idx.add(docs)
   res := idx.search("Small wild cat")
